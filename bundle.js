b07137d1936a7a68c5d3d9ba0b1829f5ff1dd53e
Pedro Oliveira <pedro.ott@hotmail.com>
2020-03-12 10:18:32 -0300

    Functions de valor pre-setado

--- bundle.js
+++ bundle.js
@@ -1,39 +1,15 @@
 "use strict";
 
-// Arrows Functions
-var array = [1, 3, 4, 5, 8, 9];
-var novoArray = array.map(function (item) {
-  return item * 2;
-});
-/*
+function somar(valorA) {
+  var valorB = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
+  console.log(valorA + valorB);
+}
 
-    1. Remove a keyword function
-    2. Colocar a flecha => entre o parentese e a chave
-    3. A função recebe parâmetro?
-        Sim
-            3.1 Remove o parentese por volta do parametro
-            .2 A função retorna apenas uma informação? (1 linha retorno)
+somar(10, 11);
 
-
-*/
-//Reduce
-
-var soma = array.reduce(function (total, proximoValor) {
-  return total + proximoValor;
-});
-
-var teste = function teste() {
-  console.log('Oi'); // NÃO É RECOMENDADO
+var soma = function soma(valorA) {
+  var valorB = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
+  return valorA + valorB;
 };
 
-var teste2 = function teste2() {
-  return [1, 2, 3];
-}; // RETORNA UM ARRAY
-
-
-var teste3 = function teste3() {
-  return {
-    nome: 'Joao das Neves',
-    idade: 10
-  };
-};
+console.log(soma(5));
--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-// Arrows Functions
-var array = [1, 3, 4, 5, 8, 9];
-
-const novoArray = array.map(item => item * 2);
-
-
-/*
-
-    1. Remove a keyword function
-    2. Colocar a flecha => entre o parentese e a chave
-    3. A função recebe parâmetro?
-        Sim
-            3.1 Remove o parentese por volta do parametro
-            .2 A função retorna apenas uma informação? (1 linha retorno)
-
-
-*/
-
-//Reduce
-
-const soma = array.reduce((total, proximoValor) => total + proximoValor);
-
-const teste = () => {
-    console.log('Oi'); // NÃO É RECOMENDADO
+function somar (valorA, valorB = 10){
+    console.log(valorA + valorB);
 }
 
-const teste2 = () => [1, 2, 3]; // RETORNA UM ARRAY
+somar(10, 11);
 
-const teste3 = () => ({nome: 'Joao das Neves', idade: 10});
+const soma = (valorA, valorB = 10) => valorA + valorB;
+
+console.log(soma(5));
